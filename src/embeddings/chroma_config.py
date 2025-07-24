01203e3d2cb95248915abbd1e5d297e6bb779846
Camila Vieira de Oliveira <camila.zero70505@gmail.com>
2025-07-24 09:56:10 -0300

    remove arquivos antigos e estrutura anterior

--- src/embeddings/chroma_config.py
+++
@@ -1,8 +0,0 @@
-import chromadb
-
-def get_chroma_client():
-    return chromadb.Client()
-
-def get_or_create_collection(name="product_embeddings"):
-    client = get_chroma_client()
-    return client.get_or_create_collection(name=name)
